af70cdd2681b39be4dde2b3690ea0bd55621cb89
remi <remi.tondeux@gmail.com>
2023-09-30 20:02:50 +0200

    avatar 100% with stimulus

---
+++ app/javascript/controllers/avatar_controller.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ['bg', 'lul']
+
+  bgtolul() {
+    this.bgTarget.classList.add('go-right')
+    this.lulTarget.classList.add('go-right')
+    this.bgTarget.classList.remove('go-left')
+    this.lulTarget.classList.remove('go-left')
+  }
+
+  lultobg() {
+    this.bgTarget.classList.add('go-left')
+    this.lulTarget.classList.add('go-left')
+    this.bgTarget.classList.remove('go-right')
+    this.lulTarget.classList.remove('go-right')
+  }
+
+  animationend() {
+    this.bgTarget.classList.toggle('backward')
+    this.lulTarget.classList.toggle('backward')
+  }
+  }
