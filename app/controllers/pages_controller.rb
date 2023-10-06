7049ba9b7f11f53f71b8ab205ae7cf189071e2fd
remi <remi.tondeux@gmail.com>
2023-10-06 14:57:15 +0200

    skills section 20% wondering how to store data without db

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,23 @@ class PagesController < ApplicationController
   end
 
   def skills
+    @backskills = {
+      'Ruby' => 7,
+      'Rails' => 8,
+      'ActiveRecord' => 5,
+      'SQL' => 4,
+      'Python' => 3,
+      'C++' => 1
+    }
+    @frontskills = {
+      'HTML' => 9,
+      'CSS' => 7,
+      'Scss' => 5,
+      'Javascript' => 5,
+      'Hotwire/Stimulus' => 6,
+      'JQuery' => 4,
+      'React' => 2
+    }
   end
 
   def philosophy
