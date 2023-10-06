6d56c1bcd0b860d7237c397779b69a214369cc9f
remi <remi.tondeux@gmail.com>
2023-10-06 19:17:27 +0200

    skills section 30% + refacto

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,23 +3,7 @@ class PagesController < ApplicationController
   end
 
   def skills
-    @backskills = {
-      'Ruby' => 7,
-      'Rails' => 8,
-      'ActiveRecord' => 5,
-      'SQL' => 4,
-      'Python' => 3,
-      'C++' => 1
-    }
-    @frontskills = {
-      'HTML' => 9,
-      'CSS' => 7,
-      'Scss' => 5,
-      'Javascript' => 5,
-      'Hotwire/Stimulus' => 6,
-      'JQuery' => 4,
-      'React' => 2
-    }
+    @skills = [back, front, devops, soft, collaboration]
   end
 
   def philosophy
@@ -38,5 +22,62 @@ class PagesController < ApplicationController
     respond_to do |format|
       format.html { render 'pages/projects/games/cosmonaut' }
     end
+  end
+
+  private
+
+  def back
+    {
+      'title' => 'skills.back',
+      'Ruby' => 7,
+      'Rails' => 8,
+      'ActiveRecord' => 5,
+      'Rspec' => 5,
+      'SQL' => 4,
+      'Python' => 3,
+      'C++' => 2
+    }
+  end
+
+  def front
+    {
+      'title' => 'skills.front',
+      'HTML' => 8,
+      'CSS' => 6,
+      'Scss' => 5,
+      'Javascript' => 5,
+      'Hotwire/Stimulus' => 6,
+      'JQuery' => 4,
+      'React' => 2
+    }
+  end
+
+  def devops
+    {
+      'title' => 'skills.devops',
+      'Gitlab CI' => 3,
+      'Docker' => 1,
+      'Kubernetes' => 1,
+      'Heroku' => 5,
+      'Jenkins' => 1,
+      'Sentry' => 4
+    }
+  end
+
+  def soft
+    {
+      'title' => 'skills.soft'
+    }
+  end
+
+  def collaboration
+    {
+      'title' => 'skills.collaboration',
+      'Gitlab / Github' => 8,
+      'Jira' => 4,
+      'Slack' => 6,
+      'Outlook' => 6,
+      'Discord' => 5
+    }
   end
 end
