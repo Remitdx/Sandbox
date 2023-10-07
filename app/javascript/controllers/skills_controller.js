832b73d5092ccf57860b6108009bf9489e2426d4
remi <remi.tondeux@gmail.com>
2023-10-07 19:24:38 +0200

    skills section 40% + skills controller js 50%

--- app/javascript/controllers/skills_controller.js
+++ app/javascript/controllers/skills_controller.js
@@ -4,15 +4,23 @@ export default class extends Controller {
   static targets = ['btnclassic', 'btnradar', 'classic', 'radar']
 
   classic() {
-    this.classicTarget.classList.remove('d-none');
-    this.radarTarget.classList.add('d-none');
+    this.classicTargets.forEach(element => {
+      element.classList.remove('d-none');
+    });
+    this.radarTargets.forEach(element => {
+      element.classList.add('d-none');
+    });
     this.btnclassicTarget.classList.add('selected');
     this.btnradarTarget.classList.remove('selected');
   }
 
   radar() {
-    this.classicTarget.classList.add('d-none');
-    this.radarTarget.classList.remove('d-none');
+    this.classicTargets.forEach(element => {
+      element.classList.add('d-none');
+    });
+    this.radarTargets.forEach(element => {
+      element.classList.remove('d-none');
+    });
     this.btnclassicTarget.classList.remove('selected');
     this.btnradarTarget.classList.add('selected');
   }
