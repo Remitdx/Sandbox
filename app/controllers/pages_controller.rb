48368834a7334741bfe745fe864d04a5a2dc4938
remi <remi.tondeux@gmail.com>
2023-10-08 18:18:34 +0200

    skills page 80%

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -29,12 +29,12 @@ class PagesController < ApplicationController
   def back
     {
       'title' => 'skills.back',
-      'Ruby' => 7,
-      'Rails' => 8,
-      'ActiveRecord' => 5,
-      'Rspec' => 5,
-      'SQL' => 4,
-      'Python' => 3,
+      'Ruby' => 8,
+      'Rails' => 9,
+      'ActiveRecord' => 7,
+      'Rspec' => 7,
+      'SQL' => 5,
+      'Python' => 4,
       'C++' => 2
     }
   end
@@ -42,23 +42,23 @@ class PagesController < ApplicationController
   def front
     {
       'title' => 'skills.front',
-      'HTML' => 8,
-      'CSS' => 6,
-      'Scss' => 5,
-      'Javascript' => 5,
-      'Hotwired' => 6,
-      'JQuery' => 4,
-      'React' => 2
+      'HTML' => 9,
+      'CSS' => 8,
+      'Scss' => 7,
+      'Javascript' => 6,
+      'Hotwired' => 7,
+      'React' => 2,
+      'JQuery' => 5
     }
   end
 
   def devops
     {
       'title' => 'skills.devops',
-      'Gitlab CI' => 3,
-      'Docker' => 1,
+      'Gitlab CI' => 4,
+      'Docker' => 2,
       'Kubernetes' => 1,
-      'Heroku' => 5,
+      'Heroku' => 7,
       'Jenkins' => 1,
       'Sentry' => 4
     }
@@ -68,7 +68,7 @@ class PagesController < ApplicationController
     {
       'title' => 'skills.soft',
       'Punctuality' => 9,
-      'Afterwork partner' => 8,
+      'Afterwork partner' => 9,
       'Leadership' => 7
     }
   end
@@ -76,19 +76,20 @@ class PagesController < ApplicationController
   def collaboration
     {
       'title' => 'skills.collaboration',
+      'Git' => 6,
       'Gitlab / Github' => 8,
-      'Jira' => 4,
-      'Slack' => 6,
-      'Outlook' => 6,
-      'Discord' => 5
+      'Jira' => 5,
+      'Slack' => 8,
+      'Outlook' => 7,
+      'Discord' => 7
     }
   end
 
   def management
     {
       'title' => 'skills.management',
-      'Project management' => 7,
-      'Team management' => 5,
+      'Project management' => 8,
+      'Team management' => 6,
       'Non-violent communication' => 9
     }
   end
