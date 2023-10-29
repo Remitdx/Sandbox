b8d909558469112f5da1370bf6226f38c46d17be
remi <remi.tondeux@gmail.com>
2023-10-29 16:28:04 +0100

    setup rspec and latest gems dependancies

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,7 +1,15 @@
-describe '#home' do
-  it { expect().to render(home) }
-  it { expect().to render(banner) }
-  it { expect().to render(navigation) }
-  it { expect().to render(footer) }
-  it { expect(age).to be within(30..77) }
+require 'rails_helper'
+
+RSpec.describe PagesController do
+  describe '#home' do
+    subject(:home) { get :home }
+
+    before { :home }
+
+    it { is_expected.to have_http_statut(:found) }
+    it { expect().to render(banner) }
+    it { expect().to render(navigation) }
+    it { expect().to render(footer) }
+    it { expect(age).to be within(30..77) }
+  end
 end
