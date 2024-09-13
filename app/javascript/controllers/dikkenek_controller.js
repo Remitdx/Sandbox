0b31ca7c2fd647551f4a77a501db25d166e945c0
remi <remi.tondeux@gmail.com>
2024-09-13 06:31:09 +0200

    dikkenek game phone design 33%

--- app/javascript/controllers/dikkenek_controller.js
+++ app/javascript/controllers/dikkenek_controller.js
@@ -1,10 +1,24 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['start', 'end']
+  static targets = ["start", "end", "characters", "quotes"]
 
   radioButtons(e) {
     e.target.nextElementSibling.checked = true;
+    this.charactersTargets.forEach(character => {
+        if ( character.nextElementSibling.checked == false ) {
+          character.classList.remove("dikkenek-selected");
+        }
+    });
+    e.target.classList.add("dikkenek-selected");
+  }
+
+  previous() {
+    console.log('PREVIOUS');
+  }
+
+  next() {
+    console.log('NEXT');
   }
 
   connect() {
