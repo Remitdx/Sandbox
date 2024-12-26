65b92df0cf46bb717b30d5a345c437a63a0738cb
remi <remi.tondeux@gmail.com>
2024-12-26 17:32:32 +0100

    add scrolling animation, toggle experiences transition

--- app/javascript/controllers/work_controller.js
+++ app/javascript/controllers/work_controller.js
@@ -4,7 +4,8 @@ export default class extends Controller {
   static targets = ['card', 'up', 'down']
 
   toggle() {
-    this.cardTarget.classList.toggle('reveal');
+    this.cardTarget.classList.toggle('hidden');
+    this.cardTarget.classList.toggle('revealed');
     this.upTarget.classList.toggle('d-none');
     this.downTarget.classList.toggle('d-none');
   }
