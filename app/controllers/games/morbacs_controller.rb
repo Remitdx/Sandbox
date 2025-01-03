a92a57f2a5522252513158f11da64552d295d50c
Remi Tondeux <remi.tondeux@gmail.com>
2025-01-03 16:54:03 +0200

    reorganise all methods in morbac model instead of controller to be rails way

--- app/controllers/games/morbacs_controller.rb
+++ app/controllers/games/morbacs_controller.rb
@@ -13,67 +13,11 @@ module Games
       @morbac = Games::Morbac.find(params[:id])
 
       if @morbac.gameover == 2 || params[:reset]
-        computed_score = params[:reset] ? reset_game : compute_turns(@morbac, params[:pos].to_i)
-        @morbac.update(value: computed_score[0], gameover: computed_score[1])
+        params[:reset] ? @morbac.reset_game : @morbac.compute_turns(params[:pos].to_i)
+        @morbac.save
       end
 
       redirect_to games_morbac_path(@morbac)
-    end
-
-    private
-
-    def reset_game
-      [ [ 5, 5, 5, 5, 5, 5, 5, 5, 5 ], 2 ]
-    end
-
-    def compute_turns(morbac, index) # return [value, gameover]
-      array1 = player_turn(morbac, index)
-      return [ morbac.value, morbac.gameover ] if array1 == []
-
-      array2 = player_win?(array1) ? array1 : ai_turn(array1)
-      gameover = ai_win?(array2) ? 1 : 2
-      gameover = player_win?(array2) ? 0 : gameover
-
-      [ array2, gameover ]
-    end
-
-    def player_turn(morbac, index) # return array modified or []
-      return [] if morbac.value[index] < 2
-
-      morbac.value[index] = 0
-      morbac.value
-    end
-
-    def player_win?(array) # return true or false
-      array[0] + array[1] + array[2] == 0 ||
-      array[3] + array[4] + array[5] == 0 ||
-      array[6] + array[7] + array[8] == 0 ||
-      array[0] + array[3] + array[6] == 0 ||
-      array[1] + array[4] + array[7] == 0 ||
-      array[2] + array[5] + array[8] == 0 ||
-      array[0] + array[4] + array[8] == 0 ||
-      array[2] + array[4] + array[6] == 0
-    end
-
-    def ai_turn(array) # return array modified
-    possible_plays = array.each_index.select { |index| array[index] == 5 }
-    return array if possible_plays == []
-
-    play = possible_plays.shuffle.first # dumb AI picking random available spot
-
-    array[play] = 1
-    array
-    end
-
-    def ai_win?(array) # return true or false
-      array[0] + array[1] + array[2] == 3 ||
-      array[3] + array[4] + array[5] == 3 ||
-      array[6] + array[7] + array[8] == 3 ||
-      array[0] + array[3] + array[6] == 3 ||
-      array[1] + array[4] + array[7] == 3 ||
-      array[2] + array[5] + array[8] == 3 ||
-      array[0] + array[4] + array[8] == 3 ||
-      array[2] + array[4] + array[6] == 3
     end
   end
 end
--- app/models/games/morbac.rb
+++ app/models/games/morbac.rb
@@ -1,4 +1,61 @@
 module Games
   class Morbac < ApplicationRecord
+
+    def reset_game
+      self.value = [ 5, 5, 5, 5, 5, 5, 5, 5, 5 ]
+      self.gameover = 2
+      self
+    end
+
+    def compute_turns(index) # return self modified
+      array = player_turn(self, index)
+      return self if array == []
+
+      self.value = player_win?(array) ? array : ai_turn(array)
+      gameover = ai_win?(self.value) ? 1 : 2
+      self.gameover = player_win?(self.value) ? 0 : gameover
+      self
+    end
+
+    private
+
+    def player_turn(morbac, index) # return array modified or []
+      return [] if morbac.value[index] < 2
+
+      morbac.value[index] = 0
+      morbac.value
+    end
+
+    def player_win?(array) # return true or false
+      array[0] + array[1] + array[2] == 0 ||
+      array[3] + array[4] + array[5] == 0 ||
+      array[6] + array[7] + array[8] == 0 ||
+      array[0] + array[3] + array[6] == 0 ||
+      array[1] + array[4] + array[7] == 0 ||
+      array[2] + array[5] + array[8] == 0 ||
+      array[0] + array[4] + array[8] == 0 ||
+      array[2] + array[4] + array[6] == 0
+    end
+
+    def ai_turn(array) # return array modified
+    possible_plays = array.each_index.select { |index| array[index] == 5 }
+    return array if possible_plays == []
+
+    play = possible_plays.shuffle.first # dumb AI picking random available spot
+
+    array[play] = 1
+    array
+    end
+
+    def ai_win?(array) # return true or false
+      array[0] + array[1] + array[2] == 3 ||
+      array[3] + array[4] + array[5] == 3 ||
+      array[6] + array[7] + array[8] == 3 ||
+      array[0] + array[3] + array[6] == 3 ||
+      array[1] + array[4] + array[7] == 3 ||
+      array[2] + array[5] + array[8] == 3 ||
+      array[0] + array[4] + array[8] == 3 ||
+      array[2] + array[4] + array[6] == 3
+    end
   end
 end
