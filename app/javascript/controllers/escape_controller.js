d3de3e6fa13ce8f6fdec290b38add970576dd51a
Remi Tondeux <remi.tondeux@gmail.com>
2025-01-09 19:26:00 +0200

    component arrow buttons correctly working with js

--- app/javascript/controllers/escape_controller.js
+++ app/javascript/controllers/escape_controller.js
@@ -1,13 +1,33 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ['']
+  static targets = ['up', 'down', 'left', 'right']
 
   connect() {
     console.log('yop !');
   }
 
   move(event) {
-    console.log(event.key);
+    switch(event.key) {
+      case "ArrowUp":
+        this.upTarget.classList.add('pressed');
+        break;
+      case "ArrowDown":
+        this.downTarget.classList.add('pressed');
+        break;
+      case "ArrowLeft":
+        this.leftTarget.classList.add('pressed');
+        break;
+      case "ArrowRight":
+        this.rightTarget.classList.add('pressed');
+        break;
+    };
+  }
+
+  keyup() {
+    this.upTarget.classList.remove('pressed');
+    this.downTarget.classList.remove('pressed');
+    this.leftTarget.classList.remove('pressed');
+    this.rightTarget.classList.remove('pressed');
   }
 }
