51768ddf8f2930aa6337301f2c50e8e0b724924a
Remi Tondeux <remi.tondeux@gmail.com>
2025-01-10 12:52:25 +0200

    sprite viewcomponent, js stimulus controller to move character in grid + collisions

--- app/components/games/maps/sprite_component.rb
+++ app/components/games/maps/sprite_component.rb
@@ -1,8 +1,10 @@
 module Games
   module Maps
     class SpriteComponent < ViewComponent::Base
-      def initialize(type:)
+      def initialize(type:, x:, y:)
         @type = type
+        @x = x
+        @y = y
       end
     end
   end
--- app/javascript/controllers/escape_controller.js
+++ app/javascript/controllers/escape_controller.js
@@ -1,30 +1,61 @@
 import { Controller } from "@hotwired/stimulus"
 
-export default class extends Controller {
-  static targets = ['up', 'down', 'left', 'right']
+const getCoordinates = (element) => {
+  const x = element.offsetTop / 32;
+  const y = element.offsetLeft / 32;
+  return [x, y]
+}
 
-  connect() {
-    console.log('yop !');
-  }
+export default class extends Controller {
+  static targets = ['up', 'down', 'left', 'right', 'grid', 'character']
 
   move(event) {
+    if (event.key == "ArrowUp" || event.key == "ArrowDown" || event.key == "ArrowLeft" || event.key == "ArrowRight") {
+      this.characterTarget.classList.remove('up', 'down', 'left', 'right');
+    }
+    const coordinates = getCoordinates(this.characterTarget);
+
     switch(event.key) {
       case "ArrowUp":
         this.upTarget.classList.add('pressed');
+        this.characterTarget.classList.add('up');
+        const nextUp = document.getElementById(`${coordinates[0] - 1}-${coordinates[1]}`);
+
+        if (nextUp.classList.contains('path')) {
+          this.characterTarget.style.top = `${this.characterTarget.offsetTop - 32}px`;
+        }
         break;
       case "ArrowDown":
         this.downTarget.classList.add('pressed');
+        this.characterTarget.classList.add('down');
+        const nextDown = document.getElementById(`${coordinates[0] + 1}-${coordinates[1]}`);
+
+        if (nextDown.classList.contains('path')) {
+          this.characterTarget.style.top = `${this.characterTarget.offsetTop + 32}px`;
+        }
         break;
       case "ArrowLeft":
         this.leftTarget.classList.add('pressed');
+        this.characterTarget.classList.add('left');
+        const nextLeft = document.getElementById(`${coordinates[0]}-${coordinates[1] - 1}`);
+
+        if (nextLeft.classList.contains('path')) {
+          this.characterTarget.style.left = `${this.characterTarget.offsetLeft - 32}px`;
+        }
         break;
       case "ArrowRight":
         this.rightTarget.classList.add('pressed');
+        this.characterTarget.classList.add('right');
+        const nextRight = document.getElementById(`${coordinates[0]}-${coordinates[1] + 1}`);
+
+        if (nextRight.classList.contains('path')) {
+          this.characterTarget.style.left = `${this.characterTarget.offsetLeft + 32}px`;
+        }
         break;
     };
   }
 
-  keyup() {
+  keyup(event) {
     this.upTarget.classList.remove('pressed');
     this.downTarget.classList.remove('pressed');
     this.leftTarget.classList.remove('pressed');
