bbf2645545bf488e11fdef303aeda0cc7f434a22
Remi Tondeux <remi.tondeux@gmail.com>
2025-01-13 11:33:28 +0200

    neon card glowing hard

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,6 @@
 //= link application.css
-//= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_tree ../images
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link popper.js
