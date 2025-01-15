23dbedf9a06b474dca91ef68fb31fe68310ee53d
Remi Tondeux <remi.tondeux@gmail.com>
2025-01-15 15:01:29 +0200

    remove minitest from application generator so rspec is free to go now

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module PortfolioV3
     config.generators do |generate|
       generate.assets false
       generate.helper false
-      generate.test_framework :test_unit, fixture: false
     end
 
     # Initialize configuration defaults for originally generated Rails version.
