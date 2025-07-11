4b533a5e959262ae26ab6e5bda38e66b8c1774f2
Remi Tondeux <remi.tondeux@gmail.com>
2025-07-11 10:38:21 +0200

    add tests for puzzle game

--- spec/models/games/puzzle_spec.rb
+++ spec/models/games/puzzle_spec.rb
@@ -1,5 +1,30 @@
-require 'rails_helper'
+module Games
+  require 'rails_helper'
 
-RSpec.describe Puzzle, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  RSpec.describe Puzzle, type: :model do
+    describe 'validations' do
+      context 'valid puzzle' do
+        subject(:valid_puzzle) { Puzzle.new(value: (0...(3 * 3)).to_a, pseudo:"JC", gameover: 2, size: 3) }
+
+        it { is_expected.to be_valid }
+      end
+
+      context 'puzzle size too big' do
+        subject(:unvalid_puzzle) { Puzzle.new(value: (0...(3 * 3)).to_a, pseudo:"JC", gameover: 2, size: 8) }
+
+        it { is_expected.to_not be_valid }
+      end
+
+      context 'puzzle without pseudo' do
+        subject(:unvalid_puzzle) { Puzzle.new(value: (0...(3 * 3)).to_a, gameover: 2, size: 5) }
+
+        it { is_expected.to_not be_valid }
+      end
+    end
+
+    describe 'functions' do
+      context '#generate_solvable_shuffle'
+      context '#compute_turn'
+    end
+  end
 end
