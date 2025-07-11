bd7088353a7978333d18a19026edbb711ee14615
Remi Tondeux <remi.tondeux@gmail.com>
2025-07-11 08:32:06 +0200

    solve bug on project pro cards + nowrap on tableofcontent title

--- app/components/cards/project_pro_card_component.rb
+++ app/components/cards/project_pro_card_component.rb
@@ -1,12 +1,12 @@
 module Cards
   class ProjectProCardComponent < ViewComponent::Base
-    def initialize(project: {})
-      @title = project[:title] || "Title?"
-      @classes = project[:classes]
-      @description = project[:description] || "missing decription"
-      @testimonials = project[:testimonials] || []
-      @period = project[:period] || "Date?"
-      @link = project[:link] || ""
+    def initialize(project: {title: "Title?", classes: "", description: "missing decription", testimonials: [], period:"Date?", link: ""})
+      @title = project["title"] || project[:title]
+      @classes = project["classes"] || project[:classes]
+      @description = project["description"] || project[:description]
+      @testimonials = project["testimonials"] || project[:testimonials]
+      @period = project["period"] || project[:period]
+      @link = project["link"] || project[:link]
     end
   end
 end
