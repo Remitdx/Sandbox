73201669ec013406863ecc4b980f17d451d41d5d
Remi Tondeux <remi.tondeux@gmail.com>
2025-07-18 15:02:20 +0200

    start tournament toolbox controller and index

---
+++ app/controllers/utilities/tournaments_controller.rb
@@ -0,0 +1,19 @@
+module Utilities
+  class TournamentsController < ApplicationController
+
+    def index
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    private
+
+  end
+end
